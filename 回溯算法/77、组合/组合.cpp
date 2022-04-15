d67389014c369789d2d8961516378a5192cb041b
24coder <62461065+24coder@users.noreply.github.com>
2022-04-15 09:27:39 +0800

    Create 组合.cpp

---
+++ 回溯算法/77、组合/组合.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+	vector<vector<int> > ans;
+	vector<int> path;
+    vector<vector<int>> combine(int n, int k) {
+		dfs(n, k, 1);
+		return ans;
+	}
+	void dfs(int n,int k,int start) {
+		if (!k) {
+			ans.push_back(path);
+			return;
+		}
+		for(int i = start; i <= n; i++) {    
+                path.push_back(i);
+				dfs(n, k-1, i+1);
+                path.pop_back();
+			}
+
+		}
+
+	};
+
+   
